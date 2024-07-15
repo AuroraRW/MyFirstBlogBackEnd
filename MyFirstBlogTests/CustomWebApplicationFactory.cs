2eec62e1c837caece3257fb076f759f1123e83da
gpoquendo <againstthaflow13@protonmail.com>
2024-07-14 22:37:56 -0600

    Enhanced testing setup and upgraded to .NET 8
    - Introduced a `.runsettings` file for NUnit to disable shadow copying, ensuring tests load assemblies from their original locations.
    - Added `CustomWebApplicationFactory` class for configuring ASP.NET Core app in integration tests, including setting up an in-memory database for `DbContextOptions`.
    - Upgraded `MyFirstBlogTests.csproj` to target .NET 8.0, updated NuGet packages, added a framework reference to `Microsoft.AspNetCore.App`, and included a project reference to the main web application.
    - Created `PostsControllerTests` class with two integration tests for validating post creation with both valid and invalid data, using a `CustomWebApplicationFactory` and `HttpClient` for making HTTP requests.

---
+++ MyFirstBlogTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MyFirstBlog.Helpers;
+using System.Linq;
+
+namespace MyFirstBlogTests;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+	protected override void ConfigureWebHost(IWebHostBuilder builder)
+	{
+		builder.ConfigureServices(services =>
+		{
+			var descriptor = services.FirstOrDefault(
+				d => d.ServiceType ==
+					typeof(DbContextOptions<DataContext>));
+
+			if (descriptor != null)
+			{
+				services.Remove(descriptor);
+			}
+
+			services.AddDbContext<DataContext>(options =>
+			{
+				options.UseInMemoryDatabase("InMemoryDbForTesting");
+			});
+		});
+	}
+}
---
+++ MyFirstBlogTests/PostsControllerTests.cs
@@ -0,0 +1,89 @@
+﻿using NUnit.Framework;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net;
+using MyFirstBlog.Dtos;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System;
+
+namespace MyFirstBlogTests
+{
+	[TestFixture]
+	public class PostsControllerTests
+	{
+		private CustomWebApplicationFactory _factory;
+		private HttpClient _client;
+
+		[SetUp]
+		public void Setup()
+		{
+			_factory = new CustomWebApplicationFactory();
+			_client = _factory.CreateClient();
+		}
+
+		[Test]
+		public async Task CreatePost_ValidData_ReturnsCreatedResponse()
+		{
+			// Arrange
+			var postData = new CreatePostDto
+			{
+				Title = "Test Title",
+				Body = "Test Content"
+			};
+			var content = new StringContent(JsonConvert.SerializeObject(postData), Encoding.UTF8, "application/json");
+
+			// Act
+			var response = await _client.PostAsync("/posts", content);
+
+			// Assert
+			Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+
+			var responseString = await response.Content.ReadAsStringAsync();
+			var responseObject = JsonConvert.DeserializeObject<PostResponse>(responseString);
+
+			Assert.IsNotNull(responseObject.Post);
+			Assert.AreEqual(postData.Title, responseObject.Post.Title);
+			Assert.AreEqual(postData.Body, responseObject.Post.Body);
+			Assert.IsNotNull(responseObject.Post.Slug);
+			Assert.IsNotNull(responseObject.Post.Id);
+			Assert.That(responseObject.Post.CreatedDate, Is.EqualTo(DateTime.UtcNow).Within(TimeSpan.FromSeconds(1)));
+		}
+
+		[Test]
+		public async Task CreatePost_InvalidData_ReturnsBadRequestResponse()
+		{
+			// Arrange
+			var postData = new CreatePostDto
+			{
+				Title = "",
+				Body = "Test Content"
+			};
+			var content = new StringContent(JsonConvert.SerializeObject(postData), Encoding.UTF8, "application/json");
+
+			// Act
+			var response = await _client.PostAsync("/posts", content);
+
+			// Assert
+			Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+
+			var responseString = await response.Content.ReadAsStringAsync();
+			var responseObject = JsonConvert.DeserializeObject<ErrorResponse>(responseString);
+
+			Assert.IsNotNull(responseObject.Errors);
+			Assert.That(responseObject.Errors, Contains.Item("Title cannot be blank"));
+		}
+	}
+
+	public class PostResponse
+	{
+		public PostDto Post { get; set; }
+	}
+
+	public class ErrorResponse
+	{
+		public string[] Errors { get; set; }
+	}
+}
