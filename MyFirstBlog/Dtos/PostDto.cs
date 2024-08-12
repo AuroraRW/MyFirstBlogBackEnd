30e0a2d1eda67bbb4516856f31dbab3ee7404764
melih <melihseyhan@gmail.com>
2024-08-11 22:36:15 -0600

    commit for assignmet_3_backend

--- MyFirstBlog/Dtos/PostDto.cs
+++
@@ -1,9 +0,0 @@
-namespace MyFirstBlog.Dtos;
-
-public record PostDto {
-    public Guid Id { get; init; }
-    public string Title { get; init; } = default!;
-    public string Slug { get; init; } = default!;
-    public string Body { get; init; } = default!;
-    public DateTime CreatedDate { get; init; }
-}
