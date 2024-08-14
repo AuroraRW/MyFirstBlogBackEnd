9da6b36fc20a4c4e4f3b57d8728e3bf5a1db7cc3
muktamyeesarker <muktamyee@yahoo.com>
2024-08-14 13:45:20 -0600

    Assignment 3 Done

---
+++ MyFirstBlog/Helpers/DatabaseHelperBasecs.cs
@@ -0,0 +1,16 @@
+﻿namespace MyFirstBlog.Helpers
+{
+    public static class DatabaseHelperBase
+    {
+
+        public static async Task ManageMigrationsAsync(IServiceProvider svcProvider)
+        {
+            //Service: An instance of db context
+            var dbContextSvc = svcProvider.GetRequiredService<DataContext>();
+
+            //Migration: This is the programmatic equivalent to Update-Database
+
+
+        }
+    }
+}
--- MyFirstBlog/Services/PostService.cs
+++ MyFirstBlog/Services/PostService.cs
@@ -1,37 +1,83 @@
-namespace MyFirstBlog.Services;
-
-using MyFirstBlog.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Dapper;
+using Npgsql;
 using MyFirstBlog.Entities;
-using System.Text.RegularExpressions;
 using MyFirstBlog.Dtos;
+using Microsoft.Extensions.Configuration;
+using MyFirstBlog.Helpers;
 
-public interface IPostService
+namespace MyFirstBlog.Services
 {
-    IEnumerable<PostDto> GetPosts();
-    PostDto GetPost(String slug);
-}
-
-public class PostService : IPostService
-{
-    private DataContext _context;
-
-    public PostService(DataContext context)
+    public class PostService
     {
-        _context = context;
-    }
+        private readonly string _connectionString;
 
-    public IEnumerable<PostDto> GetPosts()
-    {
-        return _context.Posts.Select(post => post.AsDto());
-    }
+        public PostService(IConfiguration configuration)
+        {
+            _connectionString = ConnectionHelper.GetConnectionString(configuration);
+        }
 
-    public PostDto GetPost(string slug)
-    {
-        return getPost(slug).AsDto();
-    }
+        public IEnumerable<PostDto> GetPosts()
+        {
+            using var connection = new NpgsqlConnection(_connectionString);
+            var posts = connection.Query<Post>("SELECT * FROM public.\"Posts\" ORDER BY \"Id\" ASC").ToList();
 
-    private Post getPost(string slug)
-    {
-        return _context.Posts.Where(a=>a.Slug==slug.ToString()).SingleOrDefault();
+            return posts.Select(post => new PostDto
+            {
+                Id = post.Id,
+                Title = post.Title,
+                Slug = post.Slug,
+                Body = post.Body,
+                CreatedDate = post.CreatedDate
+            }).ToList();
+        }
+
+        public PostDto GetPost(string slug)
+        {
+            using var connection = new NpgsqlConnection(_connectionString);
+            var post = connection.QuerySingleOrDefault<Post>("SELECT * FROM public.\"Posts\" WHERE \"Slug\" = @Slug", new { Slug = slug });
+            if (post == null)
+                return null;
+
+            return new PostDto
+            {
+                Id = post.Id,
+                Title = post.Title,
+                Slug = post.Slug,
+                Body = post.Body,
+                CreatedDate = post.CreatedDate
+            };
+        }
+
+        public PostDto CreatePost(CreatePostDto createPostDto)
+        {
+            var newPost = new Post
+            {
+                Id = Guid.NewGuid(),
+                Title = createPostDto.Title,
+                Slug = string.IsNullOrEmpty(createPostDto.Slug) ? GenerateSlug(createPostDto.Title) : createPostDto.Slug,
+                Body = createPostDto.Body,
+                CreatedDate = createPostDto.CreatedDate == default ? DateTime.UtcNow : createPostDto.CreatedDate
+            };
+
+            using var connection = new NpgsqlConnection(_connectionString);
+            connection.Execute("INSERT INTO public.\"Posts\" (\"Id\", \"Title\", \"Slug\", \"Body\", \"CreatedDate\") VALUES (@Id, @Title, @Slug, @Body, @CreatedDate)", newPost);
+
+            return new PostDto
+            {
+                Id = newPost.Id,
+                Title = newPost.Title,
+                Slug = newPost.Slug,
+                Body = newPost.Body,
+                CreatedDate = newPost.CreatedDate
+            };
+        }
+
+        private static string GenerateSlug(string title)
+        {
+            return title.ToLower().Replace(" ", "-");
+        }
     }
 }
